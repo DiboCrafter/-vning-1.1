f096eac02e44c7de869555b9f0a74f2339157481
DiboCrafter <emsv06002@utb.vaxjo.se>
2024-11-13 11:59:58 +0100

    Add project files.

---
+++ Övning 1/Program.cs
@@ -0,0 +1,13 @@
+﻿ussing system;
+namespace Övning1_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Du är nästan klar med kapitel 1");
+            Console.WriteLine("Du har lärt dig att skriva tecken");
+            //Good program very yes
+        }
+    }
+}
